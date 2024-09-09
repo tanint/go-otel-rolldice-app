621fb4d8ad27f4d49a583df9e83cc71e5c5d0e4e
Tanin Teerakitwattana <mypumz@gmail.com>
2024-09-09 17:51:27 +0700

    feat: rolldice produce message

--- cmd/rolldice/main.go
+++ cmd/rolldice/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/demo/rolldice/config"
 	"github.com/demo/rolldice/internal/rolldice/api"
 	"github.com/demo/rolldice/internal/rolldice/services"
 	"github.com/demo/rolldice/pkg/logger"
+	"github.com/demo/rolldice/pkg/messaging/kafka"
 	"github.com/demo/rolldice/pkg/middlewares"
 	"github.com/demo/rolldice/pkg/o11y"
 	"github.com/labstack/echo/v4"
@@ -32,7 +34,19 @@ func main() {
 
 	e.Use(middlewares.OtelMiddleware(otelConfig.AppName))
 
-	rolldiceService := services.NewRollDiceService(tracer, logger)
+	kafkaUsername := os.Getenv("KAFKA_USERNAME")
+	kafkaPassword := os.Getenv("KAFKA_PASSWORD")
+	kafkaBroker := os.Getenv("ENV_KAFKA_BROKERS")
+
+	brokers := []string{kafkaBroker}
+
+	kafkaProducer, err := kafka.NewKafkaProducer(brokers, kafkaUsername, kafkaPassword, logger, tracer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rolldiceService := services.NewRollDiceService(tracer, logger, kafkaProducer)
 
 	api.InitRolldiceHandler(e, rolldiceService)
 
